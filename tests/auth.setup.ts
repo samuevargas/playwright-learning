026b736ddd276b2ba5f5b691600d846632714fd2
Samuel Vargas <samuevargas@gmail.com>
2026-03-22 15:26:43 -0500

    Trying a different way

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -4,8 +4,8 @@ const authFile = "playwright/.auth/user.json";
 
 setup("authenticate", async ({ page }) => {
   await page.goto("https://github.com/login");
-  await page.getByLabel("Username or email address").fill(process.env.USER!);
-  await page.getByLabel("Password").fill(process.env.PASS!);
+  await page.getByLabel("Username or email address").fill("samuevargas");
+  await page.getByLabel("Password").fill("Parangari3*");
   await page.getByRole("button", { name: "Sign in" }).click();
   await page.waitForURL("https://github.com/");
 
